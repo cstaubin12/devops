89d8a0c435135d700e6277dca78fda0cb35ceea5
cstaubin12 <124001986+cstaubin12@users.noreply.github.com>
2023-04-22 17:25:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import sentry_sdk
 
 sentry_sdk.init(
-    dsn="https://74fce9d42db2443fb8e70a351c91cc8b@o4504980167524352.ingest.sentry.io/4505033051996160",
+    dsn="https://c7545b3fcce64308b622f875ff9a7ba1@o4505059889315840.ingest.sentry.io/4505059892527104",
 
     traces_sample_rate=1.0,
 )
